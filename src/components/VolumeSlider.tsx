d45c4c37146a37cd1a80af8e74bb7e05f803cff8
a-jwc <chauajw@gmail.com>
2022-03-10 23:52:48 -0800

    add VolumeSlider

---
+++ src/components/VolumeSlider.tsx
@@ -0,0 +1,31 @@
+import { SliderProp } from "../interfaces/interface";
+
+export function VolumeSlider({
+	player,
+	name,
+	fn,
+	min,
+	max,
+	defaultValue,
+	step,
+  styles
+}: SliderProp) {
+	return (
+		<div className="p-4">
+			<label>
+				{name}
+				<input
+					type="range"
+					min={min}
+					max={max}
+					defaultValue={defaultValue}
+					step={step}
+					onChange={(e) => {
+						fn(player, parseFloat(e.target.value));
+					}}
+          className="-rotate-90"
+				/>
+			</label>
+		</div>
+	);
+}
