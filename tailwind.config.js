a93cd15f27466449cd2d59b6fbcfe03a48bcff8b
a-jwc <chauajw@gmail.com>
2022-03-12 03:32:36 -0800

    add smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
 			colors: {
 				"eggshell": "#eee",
+			},
+			screens: {
+        "2xs": "360px",
+				"xs": "475px",
+				...defaultTheme.screens,
 			},
 		},
 	},
