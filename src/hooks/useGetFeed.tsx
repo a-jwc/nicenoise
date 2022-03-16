e2f87dc09c445799bdd4f622b2625492727a3576
a-jwc <chauajw@gmail.com>
2022-03-16 16:14:03 -0700

    clean up

--- src/hooks/useGetFeed.tsx
+++ src/hooks/useGetFeed.tsx
@@ -5,19 +5,20 @@ import { AuthorProp } from "../interfaces/interface";
 export const useGetSoundInfo = (url: string) => {
 	const [soundInfo, setSoundInfo] = useState<AuthorProp[]>([]);
 
-  useEffect(() => {
-    fetch(url).then(res => {
-      if (!res.ok) throw Error("Could not get sound info")
-      return res.json();
-    }).then(sounds => {
-      setSoundInfo(sounds);
-    }).catch(err => {
-      console.error(err)
-      throw Error("Error getting sound info")
-    })
-  }, [])
+	useEffect(() => {
+		fetch(url)
+			.then((res) => {
+				if (!res.ok) throw Error("Could not get sound info");
+				return res.json();
+			})
+			.then((sounds) => setSoundInfo(sounds))
+			.catch((err) => {
+				console.error(err);
+				throw Error("Error getting sound info");
+			});
+	}, []);
 
-  return soundInfo;
+	return soundInfo;
 };
 
 export const useGetOrderedSounds = (soundInfo: AuthorProp[]) => {
