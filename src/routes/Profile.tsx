96f85641cf74db834f7831869ffc5aa3d272bbd5
a-jwc <chauajw@gmail.com>
2022-03-17 02:03:26 -0700

    add liked sounds to profile

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,20 +1,24 @@
 import { useEffect, useState } from "react";
 import Container from "../components/Container";
 import Feed from "../components/Feed";
+import Playback from "../components/Playback";
 import Avatar from "../components/profile/Avatar";
 import useFetch from "../hooks/useFetch";
 import { User } from "../interfaces/interface";
+import { Sound } from "../interfaces/interface";
 
 export default function Profile() {
 	const { isLoading, apiData, error } = useFetch<User>(
 		"http://localhost:8000/api/v1/user/profile"
 	);
 
-	const [userInfo, setUserInfo] = useState({
+	const [userInfo, setUserInfo] = useState<User>({
 		username: "",
 		email: "",
 		avatar: null,
 		id: 0,
+		likes: [],
+		sounds: [],
 	});
 	const [avatar, setAvatar] = useState<Blob>();
 
@@ -22,15 +26,17 @@ export default function Profile() {
 
 	useEffect(() => {
 		if (apiData !== undefined) {
-			const data: User = apiData;
 			setUserInfo({
 				...userInfo,
 				username: apiData.username,
 				email: apiData.email,
 				avatar: apiData.avatar,
-				id: parseInt(apiData.id),
+				id: apiData.id,
+				likes: apiData.likes,
+				sounds: apiData.sounds,
 			});
 		}
+
 		fetch("http://localhost:8000/api/v1/user/get-avatar", {
 			mode: "cors",
 			credentials: "include",
@@ -46,16 +52,28 @@ export default function Profile() {
 				console.error(err);
 				throw Error("Could not get avatar");
 			});
-	}, [apiData]);
+
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [
+		apiData,
+		userInfo.avatar,
+		userInfo.email,
+		userInfo.id,
+		userInfo.username,
+		userInfo.likes,
+		userInfo.sounds,
+	]);
 
 	return (
 		<Container>
 			<div className="lg:grid lg:grid-cols-2 flex flex-col gap-8 items-center place-items-center m-12">
 				{isLoading && <span className="text-white">Loading...</span>}
 				{!isLoading && error ? (
-					<span className="text-white">You have been logged out. Please login again.</span>
+					<span className="text-white">
+						You have been logged out. Please login again.
+					</span>
 				) : (
-					<div className="bg-white bg-opacity-10 max-w-fit p-8 w-[33vw] min-w-fit justify-self-start">
+					<div className="bg-white bg-opacity-10 max-w-fit p-8 w-[33vw] min-w-fit justify-self-start lg:self-start lg:mt-10">
 						<div>
 							<div>
 								<Avatar image={avatar} />
@@ -66,14 +84,33 @@ export default function Profile() {
 						</div>
 					</div>
 				)}
-				<div>
-					{userInfo.id !== 0 ? (
-						<Feed
-							url={`http://localhost:8000/api/v1/sounds/user/${userInfo.id}?order=${order}`}
-						/>
-					) : (
-						<div></div>
-					)}
+				<div className="text-white">
+					<div>
+						<h1 className="font-bold text-xl ml-4">Sounds</h1>
+						{userInfo.sounds.length !== 0 ? (
+							<Feed
+								url={`http://localhost:8000/api/v1/sounds/user/${userInfo.id}?order=${order}`}
+							/>
+						) : (
+							<div className="flex flex-col gap-4 object-contain bg-white bg-opacity-10 p-4 m-4 min-w-fit">
+								No sounds yet!
+							</div>
+						)}
+					</div>
+					<div>
+						<h1 className="font-bold text-xl ml-4">Likes</h1>
+						{userInfo.likes.length !== 0 ? (
+							<div className="flex flex-col gap-4 object-contain bg-white bg-opacity-10 p-4 m-4 min-w-fit">
+								{userInfo.likes.map((soundInfo) => {
+									return <Playback {...soundInfo.sound} />;
+								})}
+							</div>
+						) : (
+							<div className="flex flex-col gap-4 object-contain bg-white bg-opacity-10 p-4 m-4 min-w-fit">
+								No likes yet!
+							</div>
+						)}
+					</div>
 				</div>
 			</div>
 		</Container>
